519e6a50466ae66bdbd80eb7541fc3bd3869e692
Reza Naghibi <reza@naghibi.com>
2024-11-19 12:54:53 -0500

    fjson client

---
+++ src/fjson/client/fjson_client.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2024 FiberFS
+ *
+ */
+
+#include <stdio.h>
+
+#include "fjson.h"
+
+int
+main(int argc, char **argv)
+{
+	printf("fjson_client\n");
+
+	(void)argc;
+	(void)argv;
+
+	return 0;
+}
