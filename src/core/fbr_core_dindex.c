ca4935b225796c4a9f59062680c1da34797b58da
Reza Naghibi <reza@naghibi.com>
2025-02-09 18:16:45 -0500

    Rename

--- src/core/fbr_core_dindex.c
+++ src/core/fbr_core_dindex.c
@@ -63,7 +63,7 @@ fbr_dindex_alloc(void)
 }
 
 static struct fbr_dindex_bucket *
-_dindex_hash_djb2(struct fbr_dindex *dindex, struct fbr_directory *directory)
+_dindex_get_bucket(struct fbr_dindex *dindex, struct fbr_directory *directory)
 {
 	fbr_dindex_ok(dindex);
 	fbr_directory_ok(directory);
@@ -83,7 +83,7 @@ fbr_dindex_add(struct fbr_dindex *dindex, struct fbr_directory *directory)
 	fbr_directory_ok(directory);
 	assert(directory->inode);
 
-	struct fbr_dindex_bucket *bucket = _dindex_hash_djb2(dindex, directory);
+	struct fbr_dindex_bucket *bucket = _dindex_get_bucket(dindex, directory);
 
 	assert_zero(pthread_rwlock_wrlock(&bucket->rwlock));
 	fbr_dindex_bucket_ok(bucket);
@@ -109,7 +109,7 @@ _dindex_get(struct fbr_dindex *dindex, unsigned long inode, int do_refcount)
 	find.magic = FBR_DIRECTORY_MAGIC;
 	find.inode = inode;
 
-        struct fbr_dindex_bucket *bucket = _dindex_hash_djb2(dindex, &find);
+        struct fbr_dindex_bucket *bucket = _dindex_get_bucket(dindex, &find);
 
         assert_zero(pthread_rwlock_rdlock(&bucket->rwlock));
         fbr_dindex_bucket_ok(bucket);
@@ -148,7 +148,7 @@ fbr_dindex_release(struct fbr_dindex *dindex, struct fbr_directory *directory)
 	fbr_dindex_ok(dindex);
 	fbr_directory_ok(directory);
 
-	struct fbr_dindex_bucket *bucket = _dindex_hash_djb2(dindex, directory);
+	struct fbr_dindex_bucket *bucket = _dindex_get_bucket(dindex, directory);
 
 	assert_zero(pthread_rwlock_wrlock(&bucket->rwlock));
 	fbr_dindex_bucket_ok(bucket);
