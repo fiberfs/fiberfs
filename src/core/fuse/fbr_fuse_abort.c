15d7f2230692d1a6e0a2d27deec55eae39fbc456
Reza Naghibi <reza@naghibi.com>
2025-03-17 17:11:29 -0400

    Wording

--- src/core/fuse/fbr_fuse_abort.c
+++ src/core/fuse/fbr_fuse_abort.c
@@ -13,21 +13,21 @@
 extern void fbr_test_context_abort(void);
 
 /*
- * The abort processs is meant to allow for a clean exit of all Fiber threads/processes
- * which will allow for a clean fuse unmount.
+ * The abort processs is meant to gracefully exit all Fiber threads/processes which will
+ * then allow for a clean fuse unmount.
  *
  * When a single thread/process detects an error via assert()/abort() or gets a fault or
  * external signal to exit, it dumps a backtrace and then proceeds to a context abort.
  * (See fbr_assert.c and fbr_assert.h)
  *
- * The default fbr_context_abort() behaves as follows:
+ * The default context abort (fbr_context_abort() below) behaves as follows:
  *
  * 1. If the thread is a fuse request thread, then the following steps happen:
  *    a. The Fiber context is marked as error. This signals all Fiber threads/processes that
  *       a problem exists and they will abort themselves thru this function or
  *       exit in better way (if they care). See fbr_fuse_mounted() and fbr_request_valid().
  *    b. fuse_session_exit() is called. This tells fuse to exit at its next opportunity.
- *    c. If the fuse_req is un-replied, an EIO is used as the reply.
+ *    c. If the fuse_req is un-replied, an EIO is sent back.
  *    d. pthread_exit() is called. This finishes the fuse request and allows for Fiber to
  *       continue to operate normally, albeit in a error state.
  *
@@ -41,7 +41,7 @@ extern void fbr_test_context_abort(void);
  *
  *       NOTE: all non-fuse threads/processes will block here until this step is completed.
  *
- * 3. If this is a fiber_test context, the test will exit() with an error.
+ * 3. If this is a fiber_test context, the test will abort and exit() with an error.
  *    (See fbr_test_context_abort() in fbr_test.c)
  *
  * 4. If not a fiber_test context, abort() is called. (See fbr_assert.c)
