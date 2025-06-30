941fd6689bfa7efd9176b8aa085672a359ed7221
Reza Naghibi <reza@naghibi.com>
2025-06-30 16:23:35 -0400

    Log files

---
+++ src/log/fbr_log.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024-2025 FiberFS
+ * All rights reserved.
+ *
+ */
+
+#include "fiberfs.h"
+
+void
+fbr_log_init(void)
+{
+}
---
+++ src/log/fbr_log.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024-2025 FiberFS
+ * All rights reserved.
+ *
+ */
+
+#ifndef _FBR_LOG_H_INCLUDED_
+#define _FBR_LOG_H_INCLUDED_
+
+void fbr_log_init(void);
+
+#endif /* _FBR_LOG_H_INCLUDED_ */
