17f8d9abba8f9a7bc08e7e51ce0ef86ba2fa6f07
Reza Naghibi <reza@naghibi.com>
2025-09-24 17:07:13 -0400

    Relax test

--- src/core/fuse/fbr_fuse_ctl.c
+++ src/core/fuse/fbr_fuse_ctl.c
@@ -84,6 +84,8 @@ _fuse_mount_thread(void *arg)
 	assert(ctx->session);
 	assert_zero(ctx->running);
 
+	pthread_setname_np(pthread_self(), "fiberfs_fuse_session");
+
 	struct fuse_loop_config config;
 	fbr_ZERO(&config);
 	config.max_idle_threads = 16;
