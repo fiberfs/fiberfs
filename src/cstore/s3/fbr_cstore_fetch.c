6b59d4165f2a7581896f27cfb17b3c59d65794e0
Reza Naghibi <reza@naghibi.com>
2025-10-21 19:48:29 -0400

    More splice work

--- src/cstore/s3/fbr_cstore_fetch.c
+++ src/cstore/s3/fbr_cstore_fetch.c
@@ -104,7 +104,9 @@ fbr_cstore_s3_splice_out(struct fbr_cstore *cstore, struct chttp_addr *addr, int
 	while (!fallback_rw && bytes < size) {
 		ssize_t ret = sendfile(addr->sock, fd_in, &offset, size - bytes);
 		if (ret <= 0) {
-			if (bytes == 0 && (errno == EINVAL || errno == ENOSYS)) {
+			if (!ret) {
+				break;
+			} else if (bytes == 0 && (errno == EINVAL || errno == ENOSYS)) {
 				fbr_rlog(FBR_LOG_CS_S3, "Cannot splice out, falling back");
 				cstore->cant_splice_out = 1;
 				fallback_rw = 1;
@@ -121,7 +123,9 @@ fbr_cstore_s3_splice_out(struct fbr_cstore *cstore, struct chttp_addr *addr, int
 		char buffer[FBR_CSTORE_IO_SIZE];
 		ssize_t ret = read(fd_in, buffer, sizeof(buffer));
 		if (ret <= 0) {
-			error = errno;
+			if (ret < 0) {
+				error = errno;
+			}
 			break;
 		}
 
@@ -137,6 +141,8 @@ fbr_cstore_s3_splice_out(struct fbr_cstore *cstore, struct chttp_addr *addr, int
 	fbr_rlog(FBR_LOG_CS_S3, "SPLICE_OUT wrote %zu bytes (%s) error: %d", bytes,
 		fallback_rw ? "read/write" : "sendfile", error);
 
+	// Caller will cleanup since it knows the expected byte count
+
 	return bytes;
 }
 
@@ -149,29 +155,30 @@ fbr_cstore_s3_splice_in(struct fbr_cstore *cstore, struct chttp_context *http, i
 	assert(http->state == CHTTP_STATE_BODY);
 	assert(fd_out >= 0);
 
+	// TODO we disable splice_in for now, it can lock up...
+	cstore->cant_splice_in = 1;
+
 	size_t bytes = 0;
 	int error = 0;
 	int fallback_rw = 0;
-	if (cstore->cant_splice_in || http->chunked || !size) {
+	if (cstore->cant_splice_in || http->chunked || !size || 1) {
 		fallback_rw = 1;
 	}
 
-	// TODO
-	fallback_rw = 1;
-
+	int pipe_ret = 1;
 	int pipefd[2];
 	if (!fallback_rw) {
 		assert(size == (size_t)http->length);
-		int ret = pipe(pipefd);
-		if (ret < 0) {
+		pipe_ret = pipe(pipefd);
+		if (pipe_ret < 0) {
 			fallback_rw = 1;
 		}
 	}
 
 	while (!fallback_rw && bytes < size) {
-		ssize_t pbytes1 = splice(http->addr.sock, NULL, pipefd[1], NULL, size - bytes,
+		ssize_t pipe_bytes = splice(http->addr.sock, NULL, pipefd[1], NULL, size - bytes,
 			SPLICE_F_MOVE);
-		if (pbytes1 <= 0) {
+		if (pipe_bytes < 0) {
 			if (bytes == 0 && (errno == EINVAL || errno == ENOSYS)) {
 				fbr_rlog(FBR_LOG_CS_S3, "Cannot splice in, falling back");
 				cstore->cant_splice_in = 1;
@@ -181,33 +188,43 @@ fbr_cstore_s3_splice_in(struct fbr_cstore *cstore, struct chttp_context *http, i
 				error = errno;
 			}
 			break;
-		}
-
-		ssize_t pbytes0 = 0;
-		while (pbytes0 < pbytes1) {
-			size_t obytes = splice(pipefd[0], NULL, fd_out, NULL, pbytes1 - pbytes0,
-				SPLICE_F_MOVE);
-			if (obytes <= 0) {
-				chttp_error(http, CHTTP_ERR_RESP_BODY);
-				error = errno;
-				break;
-			}
-
-			pbytes0 += obytes;
-		}
-
-		if (pbytes0 != pbytes1) {
+		} else if (!pipe_bytes) {
+			chttp_error(http, CHTTP_ERR_RESP_BODY);
 			break;
 		}
 
-		bytes += (size_t)pbytes0;
+		ssize_t out_bytes = 0;
+		while (out_bytes < pipe_bytes) {
+			ssize_t ret = splice(pipefd[0], NULL, fd_out, NULL, pipe_bytes - out_bytes,
+				SPLICE_F_MOVE);
+			if (ret <= 0) {
+				chttp_error(http, CHTTP_ERR_RESP_BODY);
+				if (ret < 0) {
+					error = errno;
+				}
+				break;
+			}
+
+			out_bytes += (size_t)ret;
+		}
+
+		if (pipe_bytes != out_bytes) {
+			chttp_error(http, CHTTP_ERR_RESP_BODY);
+			break;
+		}
+
+		bytes += (size_t)out_bytes;
+	}
+
+	if (!pipe_ret) {
+		assert_zero(close(pipefd[0]));
+		assert_zero(close(pipefd[1]));
 	}
 
 	while (fallback_rw && (!size || bytes < size)) {
 		char buffer[FBR_CSTORE_IO_SIZE];
 		size_t ret = chttp_body_read(http, buffer, sizeof(buffer));
 		if (http->error) {
-			error = http->error;
 			break;
 		} else if (ret == 0) {
 			break;
@@ -216,7 +233,6 @@ fbr_cstore_s3_splice_in(struct fbr_cstore *cstore, struct chttp_context *http, i
 		ret = fbr_sys_write(fd_out, buffer, ret);
 		if (ret == 0) {
 			chttp_error(http, CHTTP_ERR_RESP_BODY);
-			error = http->error;
 			break;
 		}
 
@@ -228,8 +244,8 @@ fbr_cstore_s3_splice_in(struct fbr_cstore *cstore, struct chttp_context *http, i
 		http->state = CHTTP_STATE_IDLE;
 	}
 
-	fbr_rlog(FBR_LOG_CS_S3, "SPLICE_IN wrote %zu bytes (%s) error: %d", bytes,
-		fallback_rw ? "read/write" : "splice", error);
+	fbr_rlog(FBR_LOG_CS_S3, "SPLICE_IN wrote %zu bytes (%s) error: %d chttp error: %d",
+		bytes, fallback_rw ? "read/write" : "splice", error, http->error);
 
 	return bytes;
 }
@@ -380,6 +396,8 @@ fbr_s3_send_put(struct fbr_cstore *cstore, struct chttp_context *http,
 		return 1;
 	}
 	assert_zero(http->length);
+
+	fbr_rlog(FBR_LOG_CS_S3, "PUT send complete, body length: %zu", length);
 
 	chttp_receive(http);
 	if (http->error) {
