d53f3899c768dcd24f78ab7ef63d94affba196d9
Reza Naghibi <reza@naghibi.com>
2026-09-11 10:47:02 -0400

    Dont assert we have a root file on the fs

--- src/core/fs/fbr_directory.c
+++ src/core/fs/fbr_directory.c
@@ -54,19 +54,15 @@ fbr_directory_root_inode_init(struct fbr_fs *fs)
 			root_file->uid = uid;
 			root_file->gid = gid;
 
-			fs->root_file = root_file;
-
 			fbr_inode_add(fs, root_file);
 
-			root_file = fbr_inode_take(fs, FBR_INODE_ROOT);
-			fbr_file_ok(root_file);
-			assert(root_file == fs->root_file);
+			fs->root_file = fbr_inode_take(fs, FBR_INODE_ROOT);
+			fbr_file_ok(fs->root_file);
+			assert_dev(fs->root_file == root_file);
 		}
 
 		fbr_fs_UNLOCK(fs);
 	}
-
-	assert_dev(fs->root_file);
 
 	fbr_inode_release(fs, &root_file);
 	assert_zero_dev(root_file);
